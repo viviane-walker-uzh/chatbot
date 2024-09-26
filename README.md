536b34c4833e71509d52a9d17c797998931a500b
viviane-walker-uzh <viviane.walker@uzh.ch>
2024-09-26 15:50:01 +0200

    docs: README, code documentation (function descriptions/docstrings)

---
+++ README.md
@@ -0,0 +1,46 @@
+---
+title: Simple_Chatbot_by_Viviane_Walker
+app_file: simple_chatbot/demo_chatbot.py
+sdk: gradio
+sdk_version: 4.42.0
+---
+
+# Simple Chatbot
+
+I created this project this autumn 2024 to illustrate that I can carry out a complete chatbot application - from development (programming) including tests to deployment on the HuggingFace Spaces website. With this project, I want to show that I can put what I have learnt from my studies into practice with my curiosity, which should illustrate my solution-oriented way of working.
+
+
+## How to use
+
+1. Make sure that you installed all required packages by creating a virtual environment with the requirements.txt.
+2. Check whether you want to execute the code on CPU (default) or to GPU (you have to adjust the code).
+3. Execute the simple_chatbot/demo_chatbot.py file OR use the simple chatbot on my HuggingFace Spaces Website.
+
+
+## Technical Specifications
+
+LLM used: [togethercomputer/RedPajama-INCITE-Chat-3B-v1 from HuggingFace](https://huggingface.co/togethercomputer/RedPajama-INCITE-Chat-3B-v1)
+
+LLM parameters:
+- max_new_tokens=50
+- do_sample=True
+- top_p=0.95
+- top_k=100
+- temperature=0.8
+- num_beams=1
+
+
+
+
+
+
+### Credits
+The idea for this chatbot and the skills to build it were aquired thanks to the course
+_Text Generation with Language Models_ by Dr. phil. Jannis Vamvas during the spring semester 2024 at the University of Zurich.
+
+The code was taken from the Gradio chatbot examples shared on their website.
+
+### Contact
+If you have any questions, do not hesitate to contact me :)
+
+
--- simple_chatbot/demo_chatbot.py
+++ simple_chatbot/demo_chatbot.py
@@ -3,32 +3,74 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, StoppingCriteria, StoppingCriteriaList, TextIteratorStreamer
 from threading import Thread
 
+# import model/tokenizer from HuggingFace
 tokenizer = AutoTokenizer.from_pretrained("togethercomputer/RedPajama-INCITE-Chat-3B-v1")
 model = AutoModelForCausalLM.from_pretrained("togethercomputer/RedPajama-INCITE-Chat-3B-v1", torch_dtype=torch.float16)
-model = model.to("cpu")
+model = model.to("cpu") # change this if you want to use GPU instead of CPU
 
 class StopOnTokens(StoppingCriteria):
+    """this custom class halts the generation process
+    when specific token IDs are encountered in the input sequence
+
+    Args:
+        StoppingCriteria (class): inherited base class
+    """
+
     def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs) -> bool:
+        """Determines whether text generation should stop based on
+        the last token in the sequence
+
+        Args:
+            input_ids (torch.LongTensor): input tensor of token IDs. The shape is expected
+                to be (batch_size, sequence_length)
+            scores (torch.FloatTensor): tensor of scores for the generated tokens. This
+                argument is provided for compatibility but is not used in this method.
+
+        Returns:
+            bool: Returns `True` if the last token in the sequence matches any of the predefined
+            `stop_ids`. In this case 29.
+        """
         stop_ids = [29, 0]
         for stop_id in stop_ids:
             if input_ids[0][-1] == stop_id:
                 return True
         return False
 
-def construct_history(message, history):
+def construct_history(message:str, history:str) -> str:
+    """constructs all previous and current message to a message history so that the LLM
+    can produce the next output based on the history (context)
+
+    Args:
+        message (str): current message/input by user
+        history (str): previous messages/conversation by user with LLM
+
+    Returns:
+        str: concatenated history with current input/message by user
+    """
     history_transformer_format = history + [[message, ""]]
     messages = "".join(["".join(["\n<human>:"+item[0], "\n<bot>:"+item[1]])
                 for item in history_transformer_format])
     return messages
 
-def predict(message, history):
+def predict(message:str, history:str) -> str:
+    """Prompts LLM for a response in a conversational context, 
+    streaming the output token-by-token as it is generated.
+
+    Args:
+        message (str): current message/input by user
+        history (str): previous messages/conversation by user with LLM
+
+    Yields:
+        str: partial generated message, updated with each new token received from the model.
+
+    """
     stop = StopOnTokens()
     model_inputs = tokenizer([construct_history(message, history)], return_tensors="pt").to("cpu")
     streamer = TextIteratorStreamer(tokenizer, timeout=10., skip_prompt=True, skip_special_tokens=True)
     generate_kwargs = dict(
         model_inputs,
         streamer=streamer,
-        max_new_tokens=40,
+        max_new_tokens=50,
         do_sample=True,
         top_p=0.95,
         top_k=100,
@@ -53,4 +95,4 @@ if __name__ == '__main__':
                             title="Simple Chatbot by Viviane Walker",
                             description="""This is a very simple chatbot to demonstrate my understanding of LLMs as well as my skills to build a very simple LLM application.
                             The default model parameters, such as the temperature, are shown in the README within the simple_chatbot repo
-                            --> https://github.com/viviane-walker-uzh/chatbot.""").launch()
+                            --> https://github.com/viviane-walker-uzh/chatbot.""").launch(share=True)
